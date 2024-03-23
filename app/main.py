1087bb68e5f26b2e6e5b79512f0c676e69ab5593
Umer902 <umerkhadim@gmail.com>
2024-03-23 15:33:47 +0500

    first

---
+++ app/main.py
@@ -0,0 +1,63 @@
+from fastapi import FastAPI
+from sqlalchemy import engine_from_config
+from sqlmodel import SQLModel, Field, create_engine, Session
+from app import settings
+from contextlib import asynccontextmanager
+
+
+class ToDo(SQLModel, table=True):
+    ID: int = Field(primary_key=True)
+    Content : str
+
+connection_string = str (settings.DATABASE_URL).replace ("postgresql", "postgresql" + "psycopg"
+)
+engine = create_engine(connection_string)
+
+def create_tables():
+    SQLModel.metadata.create_all(create_engine)
+
+@asynccontextmanager
+async def LahoreClass(app:FastAPI):
+    print("Creating table..")
+    create_tables()
+    yield 
+    
+
+app = FastAPI(lifespan = LahoreClass )
+# 
+# 
+# title="Hello World API with DB", 
+#     version="0.0.1",
+#     servers=[
+#         {
+#             "url": "http://0.0.0.0:8000", # ADD NGROK URL Here Before Creating GPT Action
+#             "description": "Development Server"
+#         }
+#         ])
+
+# def get_session():
+#     with Session(engine) as session:
+#         yield session
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+# # 1. cREAT TODOS
+# @app.post("/todo")
+# def creat_todo(todo_contect:ToDo)
+#     with Session(engine_from_config)as session:
+#         session.add(todo_content)
+#         session/commit()
+        
+        
+#         session.refresh(todo_contect)
+        
+
+
+
+# 2. TODOS
+
+
---
+++ app/settings.py
@@ -0,0 +1,7 @@
+
+from starlette.config import Config
+from starlette.datastructures import Secret
+
+Confiq = Config(".env")
+
+DATABASE_URL= "postgresql://neondb_owner:gQ0XrdlAIt9N@ep-proud-dawn-a5vi89kr.us-east-2.aws.neon.tech/lahore?sslmode=require"
